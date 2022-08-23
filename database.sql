57d08114dee5b76dac6dd953e30fb3613a232e68
Josh Kralewski <joshkmanj@gmail.com>
2022-08-23 11:37:49 -0500

    Join table created to link data between recipes and ingredients

--- database.sql
+++ database.sql
@@ -12,3 +12,11 @@ CREATE TABLE "ingredients" (
   "quantity_unit" varchar(100)
 );
 
+-- This is the join table linking the many-to-many relationship between recipes and ingredients.
+CREATE TABLE "recipe_ingredients" (
+  "id" serial primary key,
+  "recipe_id" int references "recipes", 
+  "ingredient_id" int references "ingredients",
+  "ingredient_amount" decimal(3,2),
+  "ingredient_unit" varchar(100) 
+);
