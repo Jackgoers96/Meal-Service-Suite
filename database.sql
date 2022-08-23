f7717f7b5bf96de3f06cd16100b428ab59d27dad
Josh Kralewski <joshkmanj@gmail.com>
2022-08-23 11:37:13 -0500

    Created a table for ingredients

--- database.sql
+++ database.sql
@@ -4,3 +4,11 @@ CREATE TABLE "recipes" (
   "price" decimal(3,2) 
 );
 
+CREATE TABLE "ingredients" (
+  "id" serial primary key,
+  "name" varchar(100),
+  "bulk_price" decimal(3,2),
+  "quanity" decimal(3,2),
+  "quantity_unit" varchar(100)
+);
+
