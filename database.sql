ed3235b476d5f9f0c43d82cbce4cd508cb5d35d4
Josh Kralewski <joshkmanj@gmail.com>
2022-09-01 13:35:47 -0500

    Fixed test data bugs from having too many variables inserted into queries

--- database.sql
+++ database.sql
@@ -45,39 +45,39 @@ CREATE TABLE "recipe_price" (
 ------------------------------/// Test Data ///-----------------------------
 INSERT INTO "recipes"("name","price")
 VALUES
-(1,'Chicken, mmmh',20),
-(2,'Steak Dinner',24),
-(3,'Salmon Platter',21),
-(4,'Sea bass, babyy',23),
-(5,'Vegan Delight',22);
+('Chicken, mmmh',20),
+('Steak Dinner',24),
+('Salmon Platter',21),
+('Sea bass, babyy',23),
+('Vegan Delight',22);
 
 INSERT INTO "ingredients"("name","bulk_price","quanity","quantity_unit")
 VALUES
-(1,'Chicken',20,10,'lbs'),
-(2,'Steak',50,10,'lbs'),
-(3,'Salmon',100,10,'lbs'),
-(4,'Sea Bass',50,10,'lbs'),
-(5,'Yukon Potatoes',10,10,'lbs'),
-(6,'Quinoa',8,10,'lbs'),
-(7,'Asparagus',30,10,'lbs'),
-(8,'Brussel Sprouts',25,10,'lbs'),
-(9,'Brocoli',20,10,'lbs');
+('Chicken',20,10,'lbs'),
+('Steak',50,10,'lbs'),
+('Salmon',100,10,'lbs'),
+('Sea Bass',50,10,'lbs'),
+('Yukon Potatoes',10,10,'lbs'),
+('Quinoa',8,10,'lbs'),
+('Asparagus',30,10,'lbs'),
+('Brussel Sprouts',25,10,'lbs'),
+('Brocoli',20,10,'lbs');
 
 INSERT INTO "recipe_ingredients"("recipe_id","ingredient_id","ingredient_amount","ingredient_unit")
 VALUES
-(1,1,1,10,'oz'),
-(2,1,5,10,'oz'),
-(3,1,7,10,'oz'),
-(4,2,2,10,'oz'),
-(5,2,6,10,'oz'),
-(6,2,8,10,'oz'),
-(7,3,3,10,'oz'),
-(8,3,5,10,'oz'),
-(9,3,9,10,'oz'),
-(10,4,4,10,'oz'),
-(11,4,6,10,'oz'),
-(12,4,8,10,'oz'),
-(13,5,5,10,'oz'),
-(14,5,7,10,'oz'),
-(15,5,9,10,'oz');
+(1,1,10,'oz'),
+(1,5,10,'oz'),
+(1,7,10,'oz'),
+(2,2,10,'oz'),
+(2,6,10,'oz'),
+(2,8,10,'oz'),
+(3,3,10,'oz'),
+(3,5,10,'oz'),
+(3,9,10,'oz'),
+(4,4,10,'oz'),
+(4,6,10,'oz'),
+(4,8,10,'oz'),
+(5,5,10,'oz'),
+(5,7,10,'oz'),
+(5,9,10,'oz');
 ------------------------------/// END Test Data ///-----------------------------
