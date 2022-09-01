9a7f5facb86745badc246e9b21a7c087b7eb92d4
Josh Kralewski <joshkmanj@gmail.com>
2022-09-01 15:11:14 -0500

    Added encryption module

---
+++ server/modules/encryption.js
@@ -0,0 +1,21 @@
+// No changes should be required in this file
+
+const bcrypt = require('bcryptjs');
+
+const SALT_WORK_FACTOR = 10; // This determines how secure the salt should be
+
+const encryptPassword = (password) => {
+  const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR); // This generates a random salt
+  // This next line hashes the user password and the random salt
+  // this salt and hash (and not the actual password) will then get stored in the database
+  return bcrypt.hashSync(password, salt);
+};
+
+const comparePassword = (candidatePassword, storedPassword) => {
+  return bcrypt.compareSync(candidatePassword, storedPassword);
+};
+
+module.exports = {
+  encryptPassword,
+  comparePassword,
+};
