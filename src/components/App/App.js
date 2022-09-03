7a04e598b4a62d425782957be9ec8762bca49aef
Josh Kralewski <joshkmanj@gmail.com>
2022-09-03 17:33:18 -0500

    File structure updated for public files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+
 import MealCalc from '../MealCalc/MealCalc';
-import MenuBar from '../../components/MenuBar/MenuBar'
+import MenuBar from '../MenuBar/MenuBar'
 import './App.css';
 import React from 'react';
 
